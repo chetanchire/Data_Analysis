ff4474e057f276e933577e50c99be6b4fe622919
Chetan Hire <chire@proteowise.com>
2024-12-18 14:22:50 -0500

    "rdata" was spelled as "rata" causing failure to load plot

--- server.R
+++ server.R
@@ -26,7 +26,7 @@ function(input, output, session) {
 
   output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
-    x <- rata()$S
+    x <- rdata()$S
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = "darkgray", border = "white",
